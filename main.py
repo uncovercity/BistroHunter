70f31646f0db5f127e04ffa0ca5de440dbc2fbdd
uncovercity <uncovercity@gmail.com>
2024-08-20 13:22:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-from typing import Optional
-
 from fastapi import FastAPI
+from bistrohunter import buscar_restaurantes
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Bienvenido a la API de búsqueda de restaurantes"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/restaurantes/{city}")
+async def get_restaurantes(city: str):
+    resultados = buscar_restaurantes(city)
+    
+    if isinstance(resultados, list):
+        return {
+            "resultados": [
+                {
+                    "titulo": restaurante['title'],
+                    "estrellas": restaurante.get('score', 'N/A'),
+                    "rango_de_precios": restaurante['price_range'],
+                    "url_maps": restaurante['url']
+                }
+                for restaurante in resultados
+            ]
+        }
+    else:
+        return {"mensaje": resultados}
