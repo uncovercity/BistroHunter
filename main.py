f3640414b86fc456207c1a80deabbae06b67ca84
uncovercity <uncovercity@gmail.com>
2024-08-22 16:02:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,26 +8,4 @@ async def root():
     return {"message": "Bienvenido a la API de búsqueda de restaurantes"}
 
 @app.get("/api/getRestaurants")
-async def get_restaurantes(
-    city: str, 
-    date: str = Query(None, description="La fecha en la que se planea visitar el restaurante"), 
-    price_range: str = Query(None, description="El rango de precios deseado para el restaurante"),
-    cocina: str = Query(None, description="El tipo de cocina que prefiere el cliente")
-):
-    # Asegúrate de que se pasa 'cocina' a 'buscar_restaurantes'
-    resultados = buscar_restaurantes(city, date, price_range, cocina)
-    
-    if isinstance(resultados, list):
-        return {
-            "resultados": [
-                {
-                    "nombre": restaurante['title'],
-                    "estrellas": restaurante.get('score', 'N/A'),
-                    "rango_de_precios": restaurante['price_range'],
-                    "url_maps": restaurante['url']
-                }
-                for restaurante in resultados
-            ]
-        }
-    else:
-        return {"mensaje": resultados}
+async def
