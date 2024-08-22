84947ee099bfd69262641bb4885da43c8dbc8d93
uncovercity <uncovercity@gmail.com>
2024-08-22 14:46:14 +0200

    Update bistrohunter.py

--- bistrohunter.py
+++ bistrohunter.py
@@ -136,7 +136,6 @@ def buscar_restaurantes(city: str, date: Optional[str] = None, price_range: Opti
         raise HTTPException(status_code=500, detail="Error al buscar restaurantes")
     logging.info(f"Consulta enviada a Airtable: {url} con filtro: {filter_formula}")
 
-
 @app.get("/")
 async def root():
     return {"message": "Bienvenido a la API de búsqueda de restaurantes"}
@@ -154,10 +153,10 @@ async def get_restaurantes(
         return {
             "resultados": [
                 {
-                    "titulo": restaurante['title'],
+                    "titulo": f"{restaurante.get('title', 'N/A')}. {restaurante.get('description', 'N/A')}",
                     "estrellas": restaurante.get('score', 'N/A'),
-                    "rango_de_precios": restaurante['price_range'],
-                    "url_maps": restaurante['url']
+                    "rango_de_precios": restaurante.get('price_range', 'N/A'),
+                    "url_maps": restaurante.get('url', 'N/A')
                 }
                 for restaurante in resultados
             ]
