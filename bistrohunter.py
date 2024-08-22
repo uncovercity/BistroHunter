716434551eaf9908961c274d6aa6762d0069b4f6
uncovercity <uncovercity@gmail.com>
2024-08-22 10:47:46 +0200

    Update bistrohunter.py

--- bistrohunter.py
+++ bistrohunter.py
@@ -4,6 +4,9 @@ from fastapi import FastAPI, Query, HTTPException
 from datetime import datetime
 import requests
 import logging
+from functools import wraps
+from cachetools import TTLCache
+
 app = FastAPI()
 
 # Configuración básica de logging
@@ -23,14 +26,36 @@ DAYS_ES = {
     "Saturday": "sábado",
     "Sunday": "domingo"
 }
+
 def obtener_dia_semana(fecha: datetime) -> str:
     try:
-        dia_semana_en = fecha.strftime('%A')
-        dia_semana_es = DAYS_ES.get(dia_semana_en, dia_semana_en)
+        dia_semana_en = fecha.strftime('%A')  # Obtenemos el día en inglés
+        dia_semana_es = DAYS_ES.get(dia_semana_en, dia_semana_en)  # Lo convertimos a español
         return dia_semana_es.lower()
     except Exception as e:
         logging.error(f"Error al obtener el día de la semana: {e}")
         raise HTTPException(status_code=500, detail="Error al procesar la fecha")
+
+# Create a TTL cache with a maximum of 100 items that expire after 5 minutes
+airtable_cache = TTLCache(maxsize=1000, ttl=60*30)
+
+def cache_airtable_request(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        cache_key = f"{func.__name__}:{args}:{kwargs}"
+        if cache_key in airtable_cache:
+            return airtable_cache[cache_key]
+        result = func(*args, **kwargs)
+        airtable_cache[cache_key] = result
+        return result
+    return wrapper
+
+@cache_airtable_request
+def airtable_request(url, headers, params):
+    response = requests.get(url, headers=headers, params=params)
+    return response.json() if response.status_code == 200 else None
+
+@cache_airtable_request
 def obtener_horarios(cid: str, dia_semana: str) -> Optional[bool]:
     try:
         table_name = 'Horarios'
@@ -41,19 +66,24 @@ def obtener_horarios(cid: str, dia_semana: str) -> Optional[bool]:
         params = {
             "filterByFormula": f"AND({{cid}}='{cid}', {{isOpen?}}='{dia_semana}')"
         }
-        response = requests.get(url, headers=headers, params=params)
+
+        response_data = airtable_request(url, headers, params)
         
-        if response.status_code == 200:
-            records = response.json().get('records', [])
+        if response_data:
+            records = response_data.get('records', [])
             return bool(records)
         else:
-            logging.error(f"Error al obtener horarios: {response.status_code}")
+            logging.error("Error al obtener horarios")
             return None
     except Exception as e:
         logging.error(f"Error al obtener horarios: {e}")
         raise HTTPException(status_code=500, detail="Error al obtener horarios")
+
+@cache_airtable_request
 def buscar_restaurantes(city: str, date: Optional[str] = None, price_range: Optional[str] = None, cocina: Optional[str] = None) -> Union[str, List[dict]]:
     try:
+        limit = 3  # Variable limit para limitar el número de restaurantes
+
         if date:
             fecha = datetime.strptime(date, "%Y-%m-%d")
         else:
@@ -65,14 +95,15 @@ def buscar_restaurantes(city: str, date: Optional[str] = None, price_range: Opti
         headers = {
             "Authorization": f"Bearer {AIRTABLE_PAT}",
         }
+
         formula_parts = [f"OR({{city}}='{city}', {{city_string}}='{city}')"]
+
         # Agregar la búsqueda por cocina si se proporciona
         if cocina:
             formula_parts.append(f"FIND('{cocina}', {{grouped_categories}}) > 0")
-
+    
         # Agregar el rango de precios si se proporciona
         if price_range:
-            formula_parts.append(f"FIND('{price_range}', {{price_range}}) > 0")
             formula_parts.append(f"FIND('{price_range}', ARRAYJOIN({{price_range}}, ', ')) > 0")
 
         filter_formula = "AND(" + ", ".join(formula_parts) + ")"
@@ -80,31 +111,36 @@ def buscar_restaurantes(city: str, date: Optional[str] = None, price_range: Opti
         params = {
             "filterByFormula": filter_formula
         }
-        response = requests.get(url, headers=headers, params=params)
-        
-        if response.status_code == 200:
-            records = response.json().get('records', [])
+
+        response_data = airtable_request(url, headers, params)
+        if response_data:
+            records = response_data.get('records', [])
             if not records:
                 return "No se encontraron restaurantes en la ciudad especificada."
             
             restaurantes_abiertos = []
-            for record in records:
+
+            for record in records[:limit]:
                 cid = record['fields'].get('cid')
                 if cid and obtener_horarios(cid, dia_semana):
                     restaurantes_abiertos.append(record['fields'])
             
             if restaurantes_abiertos:
-                return restaurantes_abiertos[:3]  
+                return restaurantes_abiertos  
             else:
                 return "No se encontraron restaurantes abiertos hoy."
         else:
-            return f"Error: No se pudo conectar a Airtable. Código de estado: {response.status_code}"
+            return "Error: No se pudo conectar a Airtable."
     except Exception as e:
         logging.error(f"Error al buscar restaurantes: {e}")
         raise HTTPException(status_code=500, detail="Error al buscar restaurantes")
+    logging.info(f"Consulta enviada a Airtable: {url} con filtro: {filter_formula}")
+
+
 @app.get("/")
 async def root():
     return {"message": "Bienvenido a la API de búsqueda de restaurantes"}
+
 @app.get("/api/getRestaurants")
 async def get_restaurantes(
     city: str, 
