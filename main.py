2ffafc6d32be7d5fd476b5ee829eb97fda208e31
uncovercity <uncovercity@gmail.com>
2024-08-22 14:55:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Query
 from bistrohunter import buscar_restaurantes
+from typing import Optional
 
 app = FastAPI()
 
@@ -10,22 +11,20 @@ async def root():
 @app.get("/api/getRestaurants")
 async def get_restaurantes(
     city: str, 
-    date: str = Query(None, description="La fecha en la que se planea visitar el restaurante"), 
-    price_range: str = Query(None, description="El rango de precios deseado para el restaurante"),
-    cocina: str = Query(None, description="El tipo de cocina que prefiere el cliente")
+    date: Optional[str] = Query(None, description="La fecha en la que se planea visitar el restaurante"), 
+    price_range: Optional[str] = Query(None, description="El rango de precios deseado para el restaurante"),
+    cocina: Optional[str] = Query(None, description="El tipo de cocina que prefiere el cliente")
 ):
-    # Asegúrate de que se pasa 'cocina' a 'buscar_restaurantes'
     resultados = buscar_restaurantes(city, date, price_range, cocina)
     
     if isinstance(resultados, list):
         return {
             "resultados": [
                 {
-                    "nombre": f"{restaurante.get('title', 'N/A')}. {restaurante.get('description', 'N/A')}",
-                    "cocina": restaurante['grouped_categories'],
+                    "titulo": f"{restaurante.get('title', 'N/A')}. {restaurante.get('description', 'N/A')}",
                     "estrellas": restaurante.get('score', 'N/A'),
-                    "rango_de_precios": restaurante['price_range'],
-                    "url_maps": restaurante['url']
+                    "rango_de_precios": restaurante.get('price_range', 'N/A'),
+                    "url_maps": restaurante.get('url', 'N/A')
                 }
                 for restaurante in resultados
             ]
