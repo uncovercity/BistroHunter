2d1967f9079883068b2ba9a5b97edb495bc7cb32
uncovercity <uncovercity@gmail.com>
2024-08-23 15:57:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Query, HTTPException
 from typing import Optional
-from bistrohunter import obtener_restaurantes_por_ciudad, filtrar_y_ordenar_restaurantes, obtener_dia_semana
+from bistrohunter import obtener_restaurantes_por_ciudad, obtener_dia_semana
 import logging
 from datetime import datetime
 
@@ -18,30 +18,26 @@ async def get_restaurantes(
     cocina: Optional[str] = Query(None, description="El tipo de cocina que prefiere el cliente")
 ):
     try:
+        # Convertir la fecha proporcionada a un objeto datetime
         fecha = datetime.strptime(date, "%Y-%m-%d") if date else datetime.now()
+        # Obtener el día de la semana en español
         dia_semana = obtener_dia_semana(fecha)
 
-        # Obtener los restaurantes de la ciudad
-        restaurantes = obtener_restaurantes_por_ciudad(city, price_range)
+        # Obtener los restaurantes filtrados y ordenados desde Airtable
+        restaurantes = obtener_restaurantes_por_ciudad(city, dia_semana, price_range, cocina)
         
         if not restaurantes:
-            return {"mensaje": "No se encontraron restaurantes en la ciudad especificada."}
-        
-        # Filtrar y ordenar los restaurantes
-        top_restaurantes = filtrar_y_ordenar_restaurantes(restaurantes, dia_semana, price_range, cocina)
-        
-        if not top_restaurantes:
-            return {"mensaje": "No se encontraron restaurantes abiertos con los filtros aplicados."}
+            return {"mensaje": "No se encontraron restaurantes con los filtros aplicados."}
         
         return {
             "resultados": [
                 {
                     "titulo": restaurante['fields'].get('title', 'Sin título'),
-                    "descripcion": restaurante['fields'].get('description', 'Sin descripción'),
+                    "descripcion": restaurante['fields'].get('bh_message', 'Sin descripción'),
                     "rango_de_precios": restaurante['fields'].get('price_range', 'No especificado'),
                     "puntuacion_bistrohunter": restaurante['fields'].get('score', 'N/A')
                 }
-                for restaurante in top_restaurantes
+                for restaurante in restaurantes
             ]
         }
     except Exception as e:
