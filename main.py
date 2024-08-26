c814881f61f4180b67e90662ce2f5c9e661fb400
uncovercity <uncovercity@gmail.com>
2024-08-26 21:55:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,13 +19,13 @@ async def get_restaurantes(
     diet: Optional[str] = Query(None, description="Dieta que necesita el cliente")
 ):
     try:
+        dia_semana = None
         if date:
-
             fecha = datetime.strptime(date, "%Y-%m-%d")
             dia_semana = obtener_dia_semana(fecha)
-
+        
         # Obtener los restaurantes filtrados y ordenados desde Airtable
-        restaurantes = obtener_restaurantes_por_ciudad(city, dia_semana, price_range, cocina)
+        restaurantes = obtener_restaurantes_por_ciudad(city, dia_semana, price_range, cocina, diet)
         
         if not restaurantes:
             return {"mensaje": "No se encontraron restaurantes con los filtros aplicados."}
@@ -36,7 +36,8 @@ async def get_restaurantes(
                     "titulo": restaurante['fields'].get('title', 'Sin título'),
                     "descripcion": restaurante['fields'].get('bh_message', 'Sin descripción'),
                     "rango_de_precios": restaurante['fields'].get('price_range', 'No especificado'),
-                    "puntuacion_bistrohunter": restaurante['fields'].get('score', 'N/A')
+                    "puntuacion_bistrohunter": restaurante['fields'].get('score', 'N/A'),
+                    **({"opciones_alimentarias": restaurante['fields'].get('tripadvisor_dietary_restrictions')} if diet else {})
                 }
                 for restaurante in restaurantes
             ]
