9e2c1829e00cd88386c267f9cb727513628e9145
uncovercity <uncovercity@gmail.com>
2024-12-19 15:46:21 +0100

    Update bistrohunter.py

--- bistrohunter.py
+++ bistrohunter.py
@@ -168,13 +168,31 @@ def obtener_restaurantes_por_ciudad(
                 formula_parts.append(f"OR({or_condition})")
 
         if cocina:
-            formula_parts.append(f"FIND(LOWER('{cocina}'), LOWER({{categories_string}})) > 0")
+            cocinas = cocina.split(',')
+            if len(cocinas) == 1:
+                # Mantener la fórmula original para una sola cocina
+                formula_parts.append(f"SEARCH('{cocina.strip()}', {{categories_string}}) > 0")
+            else:
+                # Crear una condición OR para múltiples cocinas
+                conditions = []
+                for c in cocinas:
+                    conditions.append(f"SEARCH('{c.strip()}', {{categories_string}}) > 0")
+                or_condition = ', '.join(conditions)
+                formula_parts.append(f"OR({or_condition})")
 
         if diet:
             formula_parts.append(f"SEARCH('{diet}', {{categories_string}}) > 0")
         
         if dish:
-            formula_parts.append(f"SEARCH('{dish}', {{google_reviews}}) > 0")
+            dishes = dish.split(',')
+            if len(dishes) ==1:
+                formula_parts.append(f"SEARCH('{dish}', {{google_reviews}}) > 0")
+            else:
+                all_conditions = [f"SEARCH('{d.strip()}', {{google_reviews}}) > 0" for d in dishes]
+                all_condition = ' AND '.join(all_conditions)
+                any_conditions = [f"SEARCH('{d.strip()}', {{google_reviews}}) > 0" for d in dishes]
+                any_condition = ' OR '.join(any_conditions)
+                formula_parts.append(f"IF({all_condition}, {all_condition}, {any_condition})")
 
         # Lista para almacenar todos los restaurantes encontrados
         restaurantes_encontrados = []
