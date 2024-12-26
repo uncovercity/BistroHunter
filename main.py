6797ec831cd1f01a547c46ec9432432de3b244b0
uncovercity <uncovercity@gmail.com>
2024-12-26 15:08:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #IMPORTS
 from fastapi import FastAPI, Query, HTTPException, Request
 from typing import Optional
-from bistrohunter import calcular_bounding_box, busqueda_coordenadas_airtable, obtener_restaurantes_por_ciudad, haversine, obtener_coordenadas, obtener_coordenadas_zona #Llama a las funciones que hemos definido en el otro archivo de código
+from bistrohunter import calcular_bounding_box, busqueda_coordenadas_airtable, obtener_restaurantes_por_ciudad, haversine, obtener_coordenadas, obtener_coordenadas_zona
 import logging
 from datetime import datetime
 
